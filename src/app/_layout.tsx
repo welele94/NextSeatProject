d7be5f1f13b47218c311728d65de184e846dcbd1
welele94 <2400005@estudante.uab.pt>
2026-06-10 00:19:33 +0100

    feat: close MVP routes in root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -15,7 +15,40 @@ export default function RootLayout() {
             backgroundColor: "#F6F7F9"
           }
         }}
-      />
+      >
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "Next Seat",
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="flight/[id]"
+          options={{
+            title: "Flight Mode",
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="learn/index"
+          options={{
+            title: "Learn More"
+          }}
+        />
+        <Stack.Screen
+          name="settings/index"
+          options={{
+            title: "Settings"
+          }}
+        />
+        <Stack.Screen
+          name="+not-found"
+          options={{
+            title: "Route not found"
+          }}
+        />
+      </Stack>
       <StatusBar style="dark" />
     </>
   );
