aa1b3e096dd79ece92716bababe2477baeeb6fe7
welele94 <2400005@estudante.uab.pt>
2026-06-12 16:37:23 +0100

    export radius

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,4 +1,5 @@
 export { colors } from "./colors";
+export { radius } from "./radius";
 export { spacing } from "./spacing";
 export { typography } from "./typography";
 export { rhythmUi } from "./rhythm";
